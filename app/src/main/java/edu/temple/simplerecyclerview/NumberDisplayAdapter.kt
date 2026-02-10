eecbbf040aac686d4af7d072e4454447bc5c0847
Jeeae <129932495+jeeae3@users.noreply.github.com>
2026-02-10 12:22:58 -0500

    error

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,13 +1,29 @@
 package edu.temple.simplerecyclerview
 
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* TODO Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter: RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>/* TODO Step 3a: Provide Adapter Parent */ {
 
     class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)= NumberViewHolder (
+        TextView(parent.context).apply {
+            textSize =22f
+        })
+
+
+    override fun onBindViewHolder(
+        holder: NumberViewHolder,
+        position: Int
+    ) {
+       holder.textView.text = ((position + 1) * 5).toString()
+    }
+
+    override fun getItemCount()=28
 
     // TODO Step 3b: Complete function definitions for adapter
 
